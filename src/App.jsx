a99460c29d5a6ffcb44f7134f66a79db0e422a14
Dinah Salazar <dinahsalazar@gmail.com>
2023-12-09 16:50:37 +0900

    Complete iteration 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import "./App.css";
+import FoodBox from "./components/FoodBox";
+import { useState } from "react";
 import foodsJson from "./foods.json";
 
 function App() {
@@ -6,6 +8,13 @@ function App() {
   return (
     <div className="App">
       <h1>LAB | React IronNutrition</h1>
+      <FoodBox food={ {
+  name: "Orange",
+  calories: 85,
+  image: "https://i.imgur.com/abKGOcv.jpg",
+  servings: 1
+} } 
+/>
     </div>
   );
 }
