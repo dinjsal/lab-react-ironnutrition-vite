1a195c853564ebf3be638589263ad2c7b4455526
Dinah Salazar <dinahsalazar@gmail.com>
2023-12-09 17:39:19 +0900

    Complete iteration 3

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,11 @@ import foodsJson from "./foods.json";
 function App() {
   const [foods, setFoods] = useState(foodsJson);
   return (
-    <div className="App">
-      <h1>LAB | React IronNutrition</h1>
-      <FoodBox food={ {
-  name: "Orange",
-  calories: 85,
-  image: "https://i.imgur.com/abKGOcv.jpg",
-  servings: 1
-} } 
-/>
+    <div>
+      <h2>IronNutrition</h2>
+      {foods.map(foodItem => {
+        return <FoodBox key={foodItem._id} food={foodItem} />;
+      })}
     </div>
   );
 }
